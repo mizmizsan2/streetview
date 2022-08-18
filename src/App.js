c15fd14b86036e55b0ef846c246566d9ab46469d
mizmizsan <hirario0615>
2022-08-18 15:39:14 +0900

    render

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import React from 'react'
+import { GoogleMap, LoadScript, StreetViewPanorama } from '@react-google-maps/api';
 
-function App() {
+const ScriptLoaded = require("../../docs/ScriptLoaded").default;
+
+const containerStyle = {
+  width: '1000px',
+  height: '1000px'
+};
+
+const center = {
+  lat: 34.398534,
+  lng: 132.48126
+};
+
+
+
+
+
+
+function MyComponent() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <LoadScript
+      googleMapsApiKey="AIzaSyA1-70vB9MlGMaod-hHgnjCOZgQhRo6Ppk"
+    >
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+      >
+        { /* Child components, such as markers, info windows, etc. */}
+        <></>
+        <StreetViewPanorama
+          position={center}
+          visible={true}
+        />
+      </GoogleMap>
+    </LoadScript>
+  )
 }
 
-export default App;
+export default React.memo(MyComponent)
